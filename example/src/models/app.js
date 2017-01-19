59eb136e64ff8a1cb5b50b21df315031794d7b6e
liekkas <zjq0717@163.com>
2017-01-19 09:26:26 +0800

    update menu bar

--- example/src/models/app.js
+++ example/src/models/app.js
@@ -1,61 +1,41 @@
+import home from '../assets/home.png'
+import heart from '../assets/heart.png'
+import spade from '../assets/spade.png'
+import diamond from '../assets/diamond.png'
+import club from '../assets/club.png'
+
 const menus = [
   {
     key: 'home',
     name: 'Home',
-    icon: 'home',
+    icon: home,
   },
   {
     key: 'heart',
     name: 'Heart Cards',
-    icon: 'heart',
+    icon: heart,
     children: [
-      {
-        key: 'heart1',
-        name: 'Heart 1',
-        icon: 'heart',
-      },
       {
         key: 'heart2',
         name: 'Heart 2',
-        icon: 'heart',
+        icon: heart,
       }
     ]
   },
   {
     key: 'spade',
     name: 'Spade Cards',
-    icon: 'heart',
-    children: [
-      {
-        key: 'spade2',
-        name: 'Spade 2',
-        icon: 'heart',
-      }
-    ]
+    icon: spade,
   },
   {
     key: 'diamond',
     name: 'Diamond Cards',
-    icon: 'heart',
-    children: [
-      {
-        key: 'diamond1',
-        name: 'Diamond 1',
-        icon: 'heart',
-      }
-    ]
+    icon: diamond,
   },
   {
     key: 'club',
     name: 'Club Cards',
-    icon: 'heart',
-    children: [
-      {
-        key: 'club1',
-        name: 'Club 1',
-        icon: 'heart',
-      }
-    ]
+    icon: club,
   }
 ]
 
